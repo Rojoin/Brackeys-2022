39da3f676f18a9fa45bd1358c8abd01ac665cbf0
Dario <otrantodario@gmail.com>
2022-08-26 12:04:06 -0300

    Menu Added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    //public void GameScene() 
+    //{
+    //    SceneManager.LoadScene("Level 1");
+    //}
+
+    public void LoadScene(string sceneName) 
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame() 
+    {
+        Application.Quit();
+    }
+}
