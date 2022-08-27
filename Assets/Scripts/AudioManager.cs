d60f41c9bff01f9ff68bd28e45da6e56338758e3
Dario <otrantodario@gmail.com>
2022-08-27 00:23:14 -0300

    Audio Manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider musicSlider;
+
+    const string MIXER_MASTER = "MasterVolume";
+
+    private void Awake()
+    {
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+    }
+
+    void SetMusicVolume(float volume) 
+    {
+        mixer.SetFloat(MIXER_MASTER, Mathf.Log10(volume) * 20);
+    }
+}
